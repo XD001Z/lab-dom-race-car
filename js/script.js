8c2c05b8dd7a2d6e6d220202f6e38a55736bf6b6
Mortimel Ojeda <ojedamorti@gmail.com>
2023-10-11 11:40:39 -0400

    Lab Done

--- js/script.js
+++ js/script.js
@@ -1,12 +1,52 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
-
+  let game;
+  
   startButton.addEventListener("click", function () {
     startGame();
   });
 
-  function startGame() {
-    console.log("start game");
+  restartButton.addEventListener("click", function () {
+    restartGame();
+  })
+
+  function restartGame() {
+    location.reload();
   }
+
+  function startGame() {
+    game = new Game();
+    game.start();
+  }
+
+  function handleKeydown(event) {
+    const key = event.key;
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+
+    if (possibleKeystrokes.includes(key)) {
+      event.preventDefault();
+
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -5;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -5;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 5;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 5;
+          break;
+      }
+    }
+  }
+  window.addEventListener("keydown", handleKeydown);
 };
